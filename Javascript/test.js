0f3bbe88e4e989b9d09f922babf2824a0664cd87
iamonkar21 <onkardate007@gmail.com>
2025-05-27 15:33:48 +0530

    update

--- Javascript/test.js
+++ Javascript/test.js
@@ -1,2 +1,7 @@
 console.log("Hello, World!");
 console.log("This is a test file.");
+
+const accountId = 1232;
+let accountName = Onkar;
+let accountEmail = "onkardate@google.com";
+var accountCity = "Pune";
