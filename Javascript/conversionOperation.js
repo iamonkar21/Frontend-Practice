9562bcdc1cbb9905bf0f1415d899f15517e2d831
iamonkar21 <onkardate007@gmail.com>
2025-05-27 15:32:55 +0530

    conversion of data types

---
+++ Javascript/conversionOperation.js
@@ -0,0 +1,26 @@
+//let score = undefined
+
+//console.log("Score is: " + typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score);
+//console.log("Value in number is: " + valueInNumber);
+//console.log(typeof valueInNumber);
+
+
+// "33" -> 33
+// "33abc" -> NaN
+// "true" -> 1
+// "false" -> 0
+
+let isLoggedIn = "Onkar"
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log("Boolean value is: " + booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log("String value is: " + stringNumber);
+console.log(typeof stringNumber);
+
