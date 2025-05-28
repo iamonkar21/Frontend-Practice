c666004a0477d65cc9dec437598fa0f04b67eb77
iamonkar21 <onkardate007@gmail.com>
2025-05-28 10:12:11 +0530

    operator

--- Javascript/conversionOperation.js
+++ Javascript/conversionOperation.js
@@ -13,14 +13,46 @@
 // "true" -> 1
 // "false" -> 0
 
-let isLoggedIn = "Onkar"
-let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log("Boolean value is: " + booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+//let isLoggedIn = "Onkar"
+//let booleanIsLoggedIn = Boolean(isLoggedIn);
+//console.log("Boolean value is: " + booleanIsLoggedIn);
+//console.log(typeof booleanIsLoggedIn);
 
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log("String value is: " + stringNumber);
-console.log(typeof stringNumber);
+//let someNumber = 33;
+//let stringNumber = String(someNumber);
+//console.log("String value is: " + stringNumber);
+//console.log(typeof stringNumber);
+
+//******************** Operations *********************
+
+//let value = 3
+//let negvalue = -value;
+//console.log("Negative value is: " + negvalue);
+
+//console.log("Value is: " + 2+2);
+//console.log(2+2);
+
+
+//let str1 = "Hello";
+//let str2 = "World";
+//let str3 = str1 + " " + str2;
+//console.log("Concatenated string is: " + str3);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log(1 + 2 + "3");   
+//console.log("3" + 1 + 2);
+//console.log("3" - 1 + 2); // "3" is converted to number, so it becomes 3 - 1 + 2 = 4
+//console.log("3" - "1" + 2); // "3" and "1" are converted to numbers, so it becomes 3 - 1 + 2 = 4
+
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+
+// let gameCounter = 100;
+// gameCounter;
+// console.log("Game Counter after increment: " + gameCounter);
 
