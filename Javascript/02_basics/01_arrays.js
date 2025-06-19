a29f1956ed264fae830bb8a6ae11aee376ebce2d
iamonkar21 <onkardate007@gmail.com>
2025-06-19 19:44:27 +0530

    array in js

---
+++ Javascript/02_basics/01_arrays.js
@@ -0,0 +1,36 @@
+//array
+
+const myArr = [0,1,2,3,4,5,6,7,8,9];
+const myHeros = ["Shaktiman", "Batman", "Superman", "Spiderman", "Ironman"];
+const myArr2 = new Array (1, 2, 3, 4);
+
+// console.log(myArr[1]);
+
+// // Array Methods
+// // push() - adds an element to the end of the array
+// myHeros.push("Hulk");   
+// console.log(myHeros);
+// myArr.push(10);
+// console.log(myArr);
+
+// // pop() - removes the last element from the array
+// myHeros.pop();
+// console.log(myHeros);
+// myArr.pop();    
+// console.log(myArr);
+
+// // unshift() - adds an element to the beginning of the array
+
+// myHeros.unshift("Captain America");
+// console.log(myHeros);
+// myArr.unshift(-1);
+// console.log(myArr);
+// myArr.unshift(55, 45, 77);
+// console.log(myArr);
+
+
+// // shift() - removes the first element from the array
+myHeros.shift();
+console.log(myHeros);
+myArr.shift();
+console.log(myArr);
